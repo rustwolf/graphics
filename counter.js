4739d51d3a56034126047a56cfb9afed3429295e
Azhar Office <azharoffice@Azhars-MacBook-Pro.local>
2024-03-27 01:25:36 +0530

    Added First commit

---
+++ counter.js
@@ -0,0 +1,13 @@
+import p5 from 'p5';
+
+console.log(p5);
+
+export function setupCounter(element) {
+  let counter = 0
+  const setCounter = (count) => {
+    counter = count
+    element.innerHTML = `count is ${counter}`
+  }
+  element.addEventListener('click', () => setCounter(counter + 1))
+  setCounter(0)
+}
